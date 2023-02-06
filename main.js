a6e1fa7d3ea31c25cd3288145cee799d27daec56
jason hernandez <jasonrhernandez721@gmail.com>
2023-02-06 12:38:31 -0500

    lunch update

--- main.js
+++ main.js
@@ -8,24 +8,34 @@ const words = ['Galaxy', 'Planet', 'Astronaut', 'Star', 'Meteor', 'Spaceship', '
 // 2. create a function to select a random word in the words array
 
 // 3. add that to randomWord array
+//
 window.onload = function() {
 
 let randomWord = [];
 function randomWordSelector() {
+    if (randomWord.length > 0) return;
     let randomIndex = Math.floor(Math.random() * words.length);
     randomWord.push(words[randomIndex]);
     document.getElementById("word").innerHTML = randomWord
 }
-
     document.getElementById("startBtn").addEventListener("click", randomWordSelector);
-};
-// 4. make a string of alphabets 
 
-
-
+// 4. make an array of alphabets 
 // 5. create a function that allows each alphabet to be a button
 
-// 6. addEventListener for a for a function to execute when button clicked.
+const keyboard = document.getElementById("keyboard");
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+alphabet.split('').forEach(letter => {
+    const key= document.createElement("button");
+    key.innerHTML = letter;
+    key.addEventListener('click', () => {
+        console.log(`key ${letter} was pressed.`);
+    });
+    keyboard.appendChild(key);
+});
+};
+
+// 6. addEventListener for a function to execute when button clicked.
 // returning thruthy or falsy depending on what answer exist in randomWord array
 
 // 7.  create function for when wrong answer a spaceman will be deducted until 0 if {
