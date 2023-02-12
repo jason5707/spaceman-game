9970afba4befd8e624e027b6fd9e92881257758c
jason hernandez <jasonrhernandez721@gmail.com>
2023-02-11 20:29:05 -0500

    reload page after win or loss

--- main.js
+++ main.js
@@ -5,18 +5,18 @@
 window.onload = function() {
 const words = ['GALAXY', 'PLANET', 'ASTRONAUT', 'STAR', 'METEOR', 'SPACESHIP', 'SATELLITE', 'COMET', 'ASTRONOMY', 'MARS' ];
 let lives = 5;
-document.getElementById("counter").innerHTML = lives;     
+document.getElementById("counter").innerHTML = lives;    
 let hiddenWord = '';
 let randomWord = [];
 let displayWord = [];
-console.log(displayWord)
+
 function randomWordSelector() {
     if (randomWord.length > 0) return;
     let randomIndex = Math.floor(Math.random() * words.length);
     // found a solution on splitting the word into an array of letters
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
     randomWord = words[randomIndex].split('');
-    console.log(randomWord)
+    
     // solution for letter to be hidden and revealed when correct
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
     displayWord = "_".repeat(randomWord.length);
@@ -35,18 +35,16 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     key.innerHTML = letter;
     key.addEventListener('click', (evt) => {
         hideButton(evt.target);
-console.log(`key ${letter} was pressed.`);
+
         randomWord.forEach((l, idx) => {
-            console.log(l);
-            console.log(letter);
+            
             if (l === letter) {
                 displayWordArray = displayWord.split('');
                 displayWordArray.splice(idx, 1, letter)
                 displayWord = displayWordArray.join('');
                
                 document.getElementById("word").innerHTML = displayWordArray.join('')
-                console.log(displayWord)
-                console.log("Correct")
+                
                 
             
         
@@ -56,8 +54,7 @@ console.log(`key ${letter} was pressed.`);
             if (!displayWord.includes(letter)) {
                 lives--;
                 document.getElementById("counter").innerHTML = lives;
-                console.log(lives);
-                console.log("Incorrect");
+               
             }
             if (lives === 0) {
                 const buttons = document.querySelectorAll("button");
@@ -66,6 +63,9 @@ console.log(`key ${letter} was pressed.`);
                 
                 document.getElementById("word").innerHTML = "TRY AGAIN";
                 document.getElementById("startBtn").style.display = "re";
+                setTimeout(function() {
+                    window.location.reload();
+                }, 5000);
             });
             }
             if (displayWord === randomWord.join('')) {
@@ -74,7 +74,11 @@ console.log(`key ${letter} was pressed.`);
                     button.style.display = "none";
                 document.getElementById("word").innerHTML = "YOU WIN";
                 
-                console.log("YOU WIN");
+                
+                
+                setTimeout(function() {
+                    window.location.reload();
+                }, 5000);
 
             })
         }
